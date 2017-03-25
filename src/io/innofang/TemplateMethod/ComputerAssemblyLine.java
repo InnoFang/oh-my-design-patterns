56cd0c7400937a4400e189324d44816f93267009
InnoFang <q984941707@qq.com>
2017-03-25 10:55:29 +0800

    TemplateMethod Learning.

--- src/io/innofang/TemplateMethod/ComputerAssemblyLine.java
+++ src/io/innofang/TemplateMethod/ComputerAssemblyLine.java
@@ -4,17 +4,17 @@ package io.innofang.TemplateMethod;
  * Created by Inno Fang on 2017/3/23.
  */
 public class ComputerAssemblyLine extends AssemblyLine {
-
+    /* 生产铝合金外壳和液晶显示屏 */
     @Override
     protected void onProduceShell() {
         System.out.println("Product Aluminum housing and Liquid Crystal Display");
     }
-
+    /* 生产元器件和键盘 */
     @Override
     protected void onProduceComponents() {
         System.out.println("Product Components and keyboard");
     }
-
+    /* 将产品打包并标上苹果标签 */
     @Override
     protected void onProductPacking() {
         System.out.println("Pack and Mark the Apple trademark");
--- src/io/innofang/TemplateMethod/RadioAssemblyLine.java
+++ src/io/innofang/TemplateMethod/RadioAssemblyLine.java
@@ -4,10 +4,9 @@ package io.innofang.TemplateMethod;
  * Created by Inno Fang on 2017/3/23.
  */
 public class RadioAssemblyLine extends AssemblyLine {
-
+    /* 生产收音机元器件和天线 */
     @Override
     protected void onProduceComponents() {
         System.out.println("Product Radio Components and Antennas");
     }
-
 }
