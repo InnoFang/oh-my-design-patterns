71e2416e8ba29c766e4e83518c81d43e2992465b
InnoFang <q984941707@qq.com>
2017-08-31 09:30:16 +0800

    update

--- src/io/innofang/builder/example/kotlin/Ferrari.kt
+++ src/io/innofang/builder/example/kotlin/Ferrari.kt
@@ -2,6 +2,8 @@ package io.innofang.builder.example.kotlin
 
 /**
  * Created by Inno Fang on 2017/8/31.
+ *
+ * You must set default value for all arguments
  */
 data class Ferrari
 private constructor(var color: String = "", var licensePlate: String = "", var brand: String = "") {
