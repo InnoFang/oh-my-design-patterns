bf5c3be186f02c1eb96c3973bcd591ccbbcc6bcd
InnoFang <q984941707@qq.com>
2017-09-05 23:10:09 +0800

    update

--- src/io/innofang/decorator/example/kotlin/DecorateCake.kt
+++ src/io/innofang/decorator/example/kotlin/DecorateCake.kt
@@ -4,8 +4,4 @@ package io.innofang.decorator.example.kotlin
  * Created by Inno Fang on 2017/9/4.
  */
 open class DecorateCake
-constructor(val cake: Cake): Cake {
-    override fun make() {
-        cake.make()
-    }
-}
+constructor(val cake: Cake): Cake by cake
