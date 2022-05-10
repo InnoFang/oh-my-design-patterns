70fe0727337934db548daeaeb87bcbc40d6aa006
InnoFang <innofang@outlook.com>
2022-05-10 21:56:56 +0800

    :sparkles: simple implementation of template method

--- cpp/template/main.cpp
+++ cpp/template/main.cpp
@@ -3,6 +3,59 @@
  * @CreateAt   : 2022/5/10
  * @Author     : Inno Fang
  * @Email      : innofang@yeah.net
- * @Description: 
+ * @Description: Simple implementation of template method
  */
 
+#include <iostream>
+
+class AbstractClass {
+public:
+    void templateMethod() const {
+        baseOperation1();
+        requireOperation1();
+        baseOperation2();
+        hook1();
+        requireOperation2();
+        hook2();
+    }
+
+protected:
+    void baseOperation1() const {
+        std::cout << "AbstractClass says: I am doing the bulk of the work" << std::endl;
+    }
+
+    void baseOperation2() const {
+        std::cout << "AbstractClass says: But I let " << std::endl;
+    }
+
+    virtual void requireOperation1() const = 0;
+
+    virtual void requireOperation2() const = 0;
+
+    virtual void hook1() const {}
+
+    virtual void hook2() const {}
+};
+
+
+class ConcreteClass : public AbstractClass {
+protected:
+    void requireOperation1() const override {
+        std::cout << "ConcreteClass says: Implemented Operation1" << std::endl;
+    }
+
+    void requireOperation2() const override {
+        std::cout << "ConcreteClass says: Implemented Operation2" << std::endl;
+    }
+
+    void hook1() const override {
+        std::cout << "ConcreteClass says: Overridden hook1" << std::endl;
+    }
+};
+
+int main() {
+    ConcreteClass *concreteClass = new ConcreteClass;
+    concreteClass->templateMethod();
+    delete concreteClass;
+    return 0;
+}
