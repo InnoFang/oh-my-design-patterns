0fc50ade465f58941adf3ab2613986eb06514268
InnoFang <innofang@outlook.com>
2022-05-16 16:53:17 +0800

    :construction: upload memento directory

---
+++ cpp/memento/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * @FileName   : memento/main.cpp
+ * @CreateAt   : 2022/5/16
+ * @Author     : Inno Fang
+ * @Email      : innofang@yeah.net
+ * @Description: 
+ */
+#include <iostream>
+
+int main() {
+    return 0;
+}
