7ad071fe3cb34a4538ac143641f1ddab57b05426
InnoFang <innofang@outlook.com>
2022-11-25 19:46:14 +0800

    :sparkles: feat: add observer directory

---
+++ cpp/observer/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * @FileName   : observer/main.cpp
+ * @CreateAt   : 2022/11/25
+ * @Author     : Inno Fang
+ * @Email      : innofang@yeah.net
+ * @Description: 
+ */
+#include <iostream>
+
+int main() {
+
+   return 0;
+}
