9dbaf8976e178d490694e71acbd4d2903020f356
InnoFang <innofang@outlook.com>
2022-11-26 22:43:07 +0800

    :tada: feat: add visitor pattern directory

---
+++ cpp/visitor/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * @FileName   : visitor/main.cpp
+ * @CreateAt   : 2022/11/26
+ * @Author     : Inno Fang
+ * @Email      : innofang@yeah.net
+ * @Description: 
+ */
+
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
